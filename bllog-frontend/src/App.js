e855ea6616f1e12e03a9c111c667ed97f77313e7
Nzioka6350 <jamesnziokamutua1@gmail.com>
2024-08-11 20:52:46 +0300

    More

--- bllog-frontend/src/App.js
+++ bllog-frontend/src/App.js
@@ -11,27 +11,17 @@ function App() {
   const currentUser = true;
   return (
     <Router>
-      <Topbar />
-      <Routes>
-        <Route exact path="/">
-          <Homepage />
-        </Route>
-        <Route path="/posts">
-          <Homepage />
-        </Route>
-        <Route path="/register">
-          {currentUser ? <Homepage /> : <Register />}
-        </Route>
-        <Route path="/login">{currentUser ? <Homepage /> : <Login />}</Route>
-        <Route path="/post/:id">
-          <Single />
-        </Route>
-        <Route path="/write">{currentUser ? <Write /> : <Login />}</Route>
-        <Route path="/settings">
-          {currentUser ? <Settings /> : <Login />}
-        </Route>
-      </Routes>
-    </Router>
+    <Topbar />
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/posts" element={<Homepage />} />
+      <Route path="/register" element={currentUser ? <Homepage /> : <Register />} />
+      <Route path="/login" element={currentUser ? <Homepage /> : <Login />} />
+      <Route path="/post/:id" element={<Single />} />
+      <Route path="/write" element={currentUser ? <Write /> : <Login />} />
+      <Route path="/settings" element={currentUser ? <Settings /> : <Login />} />
+    </Routes>
+  </Router>
   );
 }
 
