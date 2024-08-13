2b8070cad23dd88fa2d3cc20186efe7abe213afd
Nzioka6350 <jamesnziokamutua1@gmail.com>
2024-08-13 21:34:35 +0300

    Side bar

--- bllog-frontend/src/components/sidebar/Sidebar.jsx
+++ bllog-frontend/src/components/sidebar/Sidebar.jsx
@@ -7,7 +7,7 @@ export default function Sidebar() {
       <div className="sidebarItem">
         <span className="sidebarTitle">ABOUT ME</span>
         <img
-          src="https://themegoods-cdn-pzbycso8wng.stackpathdns.com/grandblog/demo/wp-content/uploads/2015/11/aboutme.jpg"
+          src="https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
           alt=""
         />
         <p>
