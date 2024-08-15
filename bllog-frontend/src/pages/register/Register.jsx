405e15b4144fe954fe866ccdb15f6b92d858b08b
Nzioka6350 <jamesnziokamutua1@gmail.com>
2024-08-15 21:03:00 +0300

    More

--- bllog-frontend/src/pages/register/Register.jsx
+++ bllog-frontend/src/pages/register/Register.jsx
@@ -10,7 +10,7 @@ export default function Register() {
         <label>Email</label>
         <input className="registerInput" type="text" placeholder="Enter your email..." />
         <label>Password</label>
-        <input className="registerInput" type="password" placeholder="Enter your password." />
+        <input className="registerInput" type="password" placeholder="Enter your password..." />
         <button className="registerButton">Register</button>
       </form>
         <button className="registerLoginButton">Login</button>
