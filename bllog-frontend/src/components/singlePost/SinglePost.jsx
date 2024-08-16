033380e5aa7fa76a1ca8870709ba4d47f74ec01d
Nzioka6350 <jamesnziokamutua1@gmail.com>
2024-08-16 21:44:18 +0300

    Posts

--- bllog-frontend/src/components/singlePost/SinglePost.jsx
+++ bllog-frontend/src/components/singlePost/SinglePost.jsx
@@ -40,7 +40,7 @@ export default function SinglePost() {
           iusto impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas
           a odit modi eos! Lorem, ipsum  sit amet consectetur adipisicing
           elit. Iste error quibusdam  quis  doloribus eos, dolore ea
-          iusto impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas
+          iusto ! Voluptatum necessitatibus eum beatae, adipisci voluptas
           a odit modi eos! Lorem, ipsum s sit amet consectetur adipisicing
           elit. Iste error quibusdam ipsa quis quidem doloribus eos, dolore ea
           iusto impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas
