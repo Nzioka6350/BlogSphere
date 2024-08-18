27105328a9efdb9f3469fed398ae3458c3a8db2f
Nzioka6350 <jamesnziokamutua1@gmail.com>
2024-08-18 11:38:10 +0300

    Posts

--- bllog-frontend/src/components/singlePost/SinglePost.jsx
+++ bllog-frontend/src/components/singlePost/SinglePost.jsx
@@ -56,7 +56,7 @@ export default function SinglePost() {
            modi eos! Lorem, ipsum dolor sit  consectetur adipisicing
           elit. Iste error quibusdam ipsa quis quidem doloribus eos, dolore ea
           iusto impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas
-          a odit modi eos! Lorem, ipsum dolor sit amet consectetur.
+          a odit modi eos Lorem, ipsum dolor sit amet consectetur.
         </p>
       </div>
     </div>
