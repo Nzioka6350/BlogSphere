6d6fae09f74f1c9cd96efb05d91ddd93907315d5
Nzioka6350 <jamesnziokamutua1@gmail.com>
2024-08-23 10:56:33 +0300

    Delivery

--- blog-app/blog-app/api/db.js
+++
@@ -1,9 +0,0 @@
-import mysql from "mysql"
-
-export const db = mysql.createConnection({
-  host:"localhost",
-  user:"root",
-  password:"MAKUENIkenya2020",
-  // password: process.env.DB_KEY,
-  database:"blogging"
-})
